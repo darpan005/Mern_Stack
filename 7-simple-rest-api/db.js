1066057ce6fa019a8653c7a19ebcba212cf3e9ab
Darpannn <darpanapatel2003@gmail.com>
2026-08-03 19:20:38 +0530

    API files updated

--- 7-simple-rest-api/db.js
+++ 7-simple-rest-api/db.js
@@ -26,15 +26,10 @@ const students = [
   },
 ];
 
-
-
-
 // Home Route
 app.get("/", (req, res) => {
   res.send("Welcome to Simple REST API!");
 });
-
-
 
 // GET All Students
 app.get("/students", (req, res) => {
@@ -54,55 +49,51 @@ app.get("/students/:id", (req, res) => {
   res.json(student);
 });
 
-
-
 //Post Route
-app.post("/students",(req,res)=>{
+app.post("/students", (req, res) => {
   const newstudent = req.body;
 
   students.push(newstudent);
 
   res.status(201).json({
-    message : "Student Adeed Succesfully",
-    student : newstudent,
+    message: "Student Adeed Succesfully",
+    student: newstudent,
   });
 });
 
-
 //PUT Route
-app.put("/students/:id",(req,res)=>{
-  const student = students.find((s)=> s.id === parseInt(req.params.id));
+app.put("/students/:id", (req, res) => {
+  const student = students.find((s) => s.id === parseInt(req.params.id));
 
-  if(!student){
+  if (!student) {
     return res.status(404).json({
-      message : "Student not Found",
+      message: "Student not Found",
     });
   }
   student.name = req.body.name;
   student.branch = req.body.branch;
 
   res.json({
-    message : "student updated successfully",
-    student : student,
+    message: "student updated successfully",
+    student: student,
   });
 });
 
-
 //Delete Route
-app.delete("/students/:id",(req,res)=>{
-  const index = students.findIndex((s)=> s.id === parseInt(req.params.id));
+app.delete("/students/:id", (req, res) => {
+  const index = students.findIndex((s) => s.id === parseInt(req.params.id));
 
-  if(index === -1){
+  if (index === -1) {
     return res.status(404).json({
-      message : "student not Found",
+      message: "student not Found",
     });
   }
 
-  const deletedstudent = students.splice(index,1);
+  const deletedstudent = students.splice(index, 1);
 
   res.json({
-    message : "student deleted successfully",
-    studnet : deletedstudent[0],
+    message: "student deleted successfully",
+    studnet: deletedstudent[0],
   });
 });
 
--- 7-simple-rest-api/server.js
+++ 7-simple-rest-api/server.js
@@ -4,33 +4,106 @@ const app = express();
 
 const PORT = 5000;
 
-const students =[
-    {
-        id:1,
-        name:"darpan",
-        branch:"CE",
-    },
-    {
-        id:2,
-        name:"rahul",
-        branch:"IT",
-    },
-    {
-        id:3,
-        name:"priya",
-        branch:"EC",
-    },
-]
+// middleware
+app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to  Rest API topic");
+//Sample Data
+const employees = [
+  {
+    id: 1,
+    name: "Darpan",
+    branch: "CE",
+    salary: 50000,
+  },
+  {
+    id: 2,
+    name: "Parth",
+    branch: "EC",
+    salary: 60000,
+  },
+  {
+    id: 3,
+    name: "Rahul",
+    branch: "IT",
+    salary: 40000,
+  },
+];
+
+// Home Route
+app.get("/", (req, res) => {
+  res.send("Welcome to Employee Page");
 });
 
-app.get("/students",(req,res)=>{
-    res.json(students);
+// Get all employees
+app.get("/employees", (req, res) => {
+  res.json(employees);
 });
 
+//Get employee by ID
+app.get("/employees/:id", (req, res) => {
+  const employee = employees.find((e) => e.id === parseInt(req.params.id));
+
+  if (!employee) {
+    return res.status(404).json({
+      message: "Employee not Found",
+    });
+  }
+  res.json(employee);
+});
+
+//Post Route
+app.post("/employees", (req, res) => {
+  const newemployee = req.body;
+
+  employees.push(newemployee);
+
+  res.status(201).json({
+    message: "Employee Added Successfully",
+    employee: newemployee,
+  });
+});
+
+//PUT Route
+app.put("/employees/:id", (req, res) => {
+  const employee = employees.find((e) => e.id === parseInt(req.params.id));
+
+  if (!employee) {
+    return res.status(404).json({
+      message: "Employee not found",
+    });
+  }
+
+  employee.name = req.body.name;
+  employee.branch = req.body.branch;
+  employee.salary = req.body.salary;
+
+  res.json({
+    message: "Employee Data Updated Successfully",
+    employee: employee,
+  });
+});
+
+//DELETE Route
+app.delete("/employees/:id", (req,res) =>{
+    const index = employees.findIndex(
+        (e) => e.id === parseInt(req.params.id)
+    );
+
+    if(index === -1){
+        return res.status(404).json({
+            message: "Employee not found",
+        });
+    }
+
+    const deletedEmployee = employees.splice(index, 1);
+
+    res.json({
+        message: "Employee deleted successfully",
+        employee: deletedEmployee[0],
+    });
+});
+
+//Start server
 app.listen(PORT,()=>{
-    console.log(`server is staarted on http://localhost:${PORT}`);
+    console.log(`Server is running on port http://localhost:${PORT}`);
 });
-
