f31820d349808d60c1690064b208b4ad8c83375d
Darpannn <darpanapatel2003@gmail.com>
2026-08-06 22:04:13 +0530

    Practice Connecting mongoDB

---
+++ 8-task-manager-backend/server.js
@@ -0,0 +1,23 @@
+const dns = require("dns");
+dns.setServers(["8.8.8.8", "1.1.1.1"]);
+
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const connectDB = require("./config/db");
+
+dotenv.config();
+
+connectDB();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
